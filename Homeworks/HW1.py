8e99a88f4d5a0b4d924282d22d467fa348d2e1ee
asimoglufatih <fatihasimoglu1998@gmail.com>
2021-03-09 21:29:28 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,16 @@
-#Explain your work
+list1 = [1,3,5,7,9]
+list2 = [0,2,4,6,8]
 
-#Question 1
-for x in range(a):
-	print(a)
+#merge the list
+newList = list1 + list2
+
+listLen = len(newList)
+
+#multiply all values
+for i in range(0,listLen):
+    newList[i] = newList[i] * 2
+
+#print all types
+for i in range(0,listLen):
+    print(type(newList[i]))
+
