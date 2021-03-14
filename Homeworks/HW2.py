e8a0081768d664513d973867f725ea2707ec2428
asimoglufatih <fatihasimoglu1998@gmail.com>
2021-03-14 21:09:12 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,37 @@
-#Explain your work
+#These cv`s are taken from The Sopranos Series.
 
-#Question 1
-for x in range(a):
-	print(a)
+cvs = {
+
+       'cv1' : {'name': "Anthony",
+              'surname': "Soprano",
+              'title': "Boss",
+              'age': "42"},
+
+       'cv2' : {'name' : "Carmela",
+              'surname': "Soprano",
+              'title' : "housewife",
+              'age' : "38"},
+    
+       'cv3' : {'name' : "Paulie",
+              'surname': "Gualtieri",
+              'title' : "caporegime",
+              'age' : "60"},
+
+       'cv4' : {'name' : "Silvio",
+              'surname' : "Dante",
+              'title' : "Consigliere",
+              'age' : "52"},
+
+
+       'cv5' : {'name' : "Arthur",
+              'surname' : "Bucco",
+              'title' : "restaurateur",
+              'age' : "39"}
+      
+
+}
+
+for k,v in cvs.items():
+    print(f'{k} = ')
+    for key, value in v.items():
+           print(f'{key} : {value}')
